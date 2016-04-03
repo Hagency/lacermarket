fbf70f5163cc1ae7a0d3b4ffba910ccdf043fc1c
Bruce99H <f3.calarco@gmail.com>
2016-04-03 20:55:17 +0200

    user autentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330225540) do
+ActiveRecord::Schema.define(version: 20160403162547) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
