3ddca93bc5320165838d6b2dbc22d11a2ef077d1
Bruce99H <f3.calarco@gmail.com>
2016-05-09 16:35:31 +0200

    test

--- db/migrate/20160404174332_add_slug_to_contents.rb
+++ db/migrate/20160404174332_add_slug_to_contents.rb
@@ -2,6 +2,6 @@ class AddSlugToContents < ActiveRecord::Migration
   def change
     add_column :contents, :name, :string
     add_column :contents, :slug, :string
-    add_index :contents, :slug, uniq: true
+    add_index :contents, :slug, unique: true
   end
 end
